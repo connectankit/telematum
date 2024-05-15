4b0f5bd79e31ad47172dc6dcab319d164cd71b34
Ankit Kumar <70808441+akhacker0001@users.noreply.github.com>
2024-05-15 15:02:59 +0530

    donw

--- src/components/TableData.jsx
+++ src/components/TableData.jsx
@@ -21,7 +21,8 @@ const TableData = () => {
           <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
               <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-gray-400">
-                Today's Appointment List For Hitesh Bro
+                {/* Today's Appointment List For Hitesh Bro */}
+                hello world
               </caption>
               <thead
                 class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
